e01ec8597ac404df6a7dd73a76720aff49d580f9
anthonychemaly <anthonychemalygr@gmail.com>
2021-03-23 18:20:07 +0200

    ds

--- src/components/WidgetBar/index.js
+++ src/components/WidgetBar/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { GiSharpSmile } from "react-icons/gi";
 import { FaCouch } from "react-icons/fa";
 import { FcSportsMode } from "react-icons/fc";
